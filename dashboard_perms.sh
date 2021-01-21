0f470e75764681edcd3d3730cf584ec0749dbf7d
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2021-01-21 09:59:16 +0000

    Delete dashboard_perms.sh

--- dashboard_perms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-stripes mod add --tenant diku
-stripes mod enable --tenant diku
-stripes okapi login diku_admin admin --tenant diku
-stripes perm assign --name module.dashboard.enabled  --user diku_admin
-stripes perm assign --name settings.dashboard.enabled  --user diku_admin
-stripes perm assign --name servint.settings.write  --user diku_admin
-stripes perm assign --name servint.refdata.write  --user diku_admin
