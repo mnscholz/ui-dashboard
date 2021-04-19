b532e06b36a45db0adfeb7a8d4ce9f1712adc86b
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-04-19 14:24:37 +0100

    experimental usage of registry plugin

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { lazy, Suspense } from 'react';
 import { Switch } from 'react-router-dom';
 import { Route } from '@folio/stripes/core';
 import PropTypes from 'prop-types';
+import Registry from '@folio/plugin-resource-registry';
 
 const Settings = lazy(() => import('./settings'));
 const DashboardsRoute = lazy(() => import('./routes/DashboardsRoute'));
@@ -9,9 +10,11 @@ const DashboardRoute = lazy(() => import('./routes/DashboardRoute'));
 const DashboardOrderRoute = lazy(() => import('./routes/DashboardOrderRoute'));
 const WidgetCreateRoute = lazy(() => import('./routes/WidgetCreateRoute'));
 
+const registry = Registry;
+registry.registerResource('widget');
+
 const App = (appProps) => {
   const { actAs, match: { path } } = appProps;
-
   if (actAs === 'settings') {
     return (
       <Suspense fallback={null}>
@@ -19,6 +22,27 @@ const App = (appProps) => {
       </Suspense>
     );
   }
+
+  const agreementFilterParams = {
+    filters: [
+      {
+        filter: 'agremeentStatus',
+        value: 'active'
+      },
+      {
+        filter: 'agreementStatus',
+        value: 'draft'
+      }
+    ],
+    sort: 'name'
+  };
+
+  const agreementReg = registry.getResource('agreement');
+  console.log('Agreement getAllTemplate: %o', agreementReg.performViewAllTemplate(agreementFilterParams));
+  console.log('Agreement resource by id: %o', agreementReg.performViewTemplate({ id: '12345' }));
+
+  const alReg = registry.getResource('agreementLine');
+  console.log('Agreement line resource by id', alReg.performViewTemplate({ id: "54321", owner: { id: "abcde" } }))
 
   return (
     <Suspense fallback={null}>
