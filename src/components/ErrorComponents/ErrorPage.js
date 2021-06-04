4a5aa7a68b2cbe17deb624f636f5c6b6e6b6e0bd
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2021-06-04 15:26:50 +0100

    "ERM-1657 Implemented "dashboard not found" error" (#65)
    * "ERM-1657 Implemented "dashboard not found" error"
    
    * Removed unnecessary line
    
    Co-authored-by: EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>

--- src/components/ErrorComponents/ErrorPage.js
+++ src/components/ErrorComponents/ErrorPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
 
 import css from './Error.css';
 
@@ -8,9 +7,6 @@ export default function ErrorPage({ children }) {
   return (
     <div className={css.errorPage}>
       <span className={css.errorMessageLabel}>
-        <div>
-          <FormattedMessage id="ui-dashboard.error" />
-        </div>
         <div>
           {children}
         </div>
