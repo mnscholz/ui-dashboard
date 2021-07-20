e01d1e3df589835415658ef49a0682783d9a377b
aditya matukumalli <aditya.matukumalli@gmail.com>
2021-07-20 14:46:55 -0400

    Update date validation message, refs ERM-1702 (#92)
    * Update date validation message, refs ERM-1702
    
    * cleanup
    
    * lint
    
    * lint
    
    Co-authored-by: Aditya Matukumalli <am693@LIB-M-0M1HV2R.local>

--- src/components/TypeComponents/SimpleSearch/Form/filters/SimpleSearchDateFilterField.js
+++ src/components/TypeComponents/SimpleSearch/Form/filters/SimpleSearchDateFilterField.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, useIntl } from 'react-intl';
-
 import { Field, useForm, useFormState } from 'react-final-form';
 
 import { get } from 'lodash';
@@ -9,6 +8,7 @@ import { get } from 'lodash';
 import {
   Col,
   Datepicker,
+  getLocaleDateFormat,
   KeyValue,
   Row,
   Select,
@@ -54,7 +54,7 @@ const SimpleSearchDateFilterField = ({
 
   const dateValidator = (value, allValues) => {
     if (get(allValues, `${name}.relativeOrAbsolute`) === 'absolute' && !value) {
-      return <FormattedMessage id="ui-dashboard.simpleSearchForm.filters.dateFilterField.absoluteValueWarning" />;
+      return <FormattedMessage id="ui-dashboard.simpleSearchForm.filters.dateFilterField.invalidDate" values={{ dateFormat: getLocaleDateFormat({ intl }) }} />;
     }
     return undefined;
   };
