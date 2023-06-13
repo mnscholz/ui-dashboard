96803e15004ccdca1ed96f2dfe845827a878c516
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2023-06-13 11:51:54 +0100

    fix: URI Validation (#231)
    Outsourced the "is this a valid URL" question to javascript/validator library, so that responsibility for upkeeping security is not on our shoulders.
    
    SI-23

--- src/components/TypeComponents/utilities/validators.js
+++ src/components/TypeComponents/utilities/validators.js
@@ -1,13 +1,29 @@
 import { FormattedMessage } from 'react-intl';
+import isURL from 'validator/lib/isURL';
+
 import { Tooltip, IconButton } from '@folio/stripes/components';
 
 const validateURL = (value) => {
   if (value) {
-    const whiteList = ['/', 'http://', 'https://'];
-    const blackList = /[<>"'\\{}|^[\]`%;?@=#!]/;
-    if (!whiteList.some(pattern => value.startsWith(pattern)) || value.match(blackList)) {
+    const protocolWhitelist = ['/', 'http://', 'https://'];
+
+    // We do this because the entered URL can be a local page link starting with /
+    const protocolMatch = protocolWhitelist.some(pattern => value.startsWith(pattern));
+
+    /*
+      If the page link DOES start with '/' we need to check it's a valid URL still,
+      so fill in https://test.com as a filler baseUrl for validation.
+
+      Use isURL from third party library validator to handle complicated regex checking
+      of URLs to get rid of javascript attacks
+    */
+    const encoded = encodeURI(value);
+    const parsed = new URL(encoded, 'http://test.com');
+
+    if (!protocolMatch || !isURL(parsed.toString())) {
       return (
-        <><FormattedMessage id="ui-dashboard.simpleSearchForm.error.invalidURL" />
+        <>
+          <FormattedMessage id="ui-dashboard.simpleSearchForm.error.invalidURL" />
           <Tooltip
             id="tooltipValidateUrl"
             text={<FormattedMessage id="ui-dashboard.simpleSearchForm.error.invalidURL.tooltip" />}
@@ -22,10 +38,11 @@ const validateURL = (value) => {
           </Tooltip>
         </>
       );
-    } else {
-      // eslint-disable-next-line no-param-reassign
-      value = encodeURI(value);
     }
+    /* Have removed the logic which sets value to encodeURI here.
+     * Instead we can encodeURI when reading from DB. Should NOT do both,
+     * and in this direction we are protected from directly DB inserted URLs
+     */
   }
   return undefined;
 };
